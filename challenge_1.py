9c44e4048d5c291bec6d3937796ab457e2af18fb
jeff <jeff.etale@student.moringaschool.com>
2023-08-19 08:25:26 +0300

    defined a function to convert time in 12hr to 24hr

--- challenge_1.py
+++ challenge_1.py
@@ -1,8 +1,19 @@
-'''
+"""
 Challenge 1: Converting 12-hour time to 24-hour time
 Converting a 12-hour time like "8:30 am" or "8:30 pm" to 24-hour time (like "0830" or "2030") sounds easy enough, right? Well, let's see if you can do it!
 
 You will have to define a function, which will be given an hour (always in the range of 1 to 12, inclusive), a minute (always in the range of 0 to 59, inclusive), and a period (either "am" or "pm") as input.
 
 Your task is to return a four-digit string that encodes that time in 24-hour time.
-'''
+"""
+
+
+def to_24_hour(hour, minute, period):
+    if period == "pm" and hour != 12:
+        hour += 12
+    elif period == "am" and hour == 12:
+        hour = 0
+    return f"{hour:02d}{minute:02d}"
+
+
+print(to_24_hour(8, 30, "pm"))
