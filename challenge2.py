dbdd002d9947096b5a87f7f02431d406500ccc5e
jeff <jeff.etale@student.moringaschool.com>
2023-08-19 07:35:56 +0300

    created repo and added python files

---
+++ challenge2.py
@@ -0,0 +1,6 @@
+'''
+Challenge 2: Two numbers are positive.
+Task:
+Your job is to write a function, which takes three integers a, b, and c as arguments, and returns True if exactly two of of the three
+ integers are positive numbers (greater than zero), and False - otherwise.
+'''
---
+++ challenge_1.py
@@ -0,0 +1,8 @@
+'''
+Challenge 1: Converting 12-hour time to 24-hour time
+Converting a 12-hour time like "8:30 am" or "8:30 pm" to 24-hour time (like "0830" or "2030") sounds easy enough, right? Well, let's see if you can do it!
+
+You will have to define a function, which will be given an hour (always in the range of 1 to 12, inclusive), a minute (always in the range of 0 to 59, inclusive), and a period (either "am" or "pm") as input.
+
+Your task is to return a four-digit string that encodes that time in 24-hour time.
+'''
