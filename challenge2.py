c90788c708f3d9921042f1d5e6f5054dd70a9c10
jeff <jeff.etale@student.moringaschool.com>
2023-08-19 11:24:29 +0300

    created a function that returns true for only 2 positive integers

--- challenge2.py
+++ challenge2.py
@@ -1,6 +1,11 @@
-'''
+"""
 Challenge 2: Two numbers are positive.
 Task:
 Your job is to write a function, which takes three integers a, b, and c as arguments, and returns True if exactly two of of the three
  integers are positive numbers (greater than zero), and False - otherwise.
-'''
+"""
+
+def two_positive(a: int, b: int, c: int):
+    return sum(x > 0 for x in (a, b, c)) == 2
+
+print(two_positive(3,4,0))
